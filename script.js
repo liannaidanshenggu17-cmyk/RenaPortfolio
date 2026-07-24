fc0d2b8e37f25ab0fb691c15d3139c69cb3f1eef
unasesoumu <liannaidanshenggu17@gmail.com>
2026-07-24 12:51:27 +0900

    Fix punctuation in works array
    Corrected punctuation in work name and text.

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const results = document.getElementById("searchResults");
 
 const works = [
     {
-        name: "「またのお越しはお待ちしておりません」。",
-        text: "CoC6版 ソロシナリオ 海底郵便局を舞台にした物語作品。",
+        name: "「またのお越しはお待ちしておりません。」",
+        text: "CoC6版 ソロシナリオ 海底郵便局を舞台にした物語作品",
         url: "work1.html"
     }
 ];
